21d77fced066dc597eec5583dfbcdad55b9dc5e2
dj <1957588051@qq.com>
2019-06-18 20:54:59 +0800

    添加工程到本地仓库中

---
+++ src/com/itheima/Test.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * @Author: 邓杰
+ * @Date: 2019/6/18 20:53
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("happy");
+    }
+}
